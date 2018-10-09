0bdd61e07edf2edd77854d31d325ef71188cd212
spafac <developer@spafac.com>
2018-10-10 06:06:57 +0900

    amendment

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  actions :all, except: [:create, :new, :update, :edit, :destroy]
+  actions :all, except: [:create, :new, :update,]
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
